27b4a632fc81223644ee20232cefe917374ce5b6
UjinAra <aev_co@mail.ru>
2022-11-27 02:02:36 +0700

    Пример 25

---
+++ 25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using static System.Console;
+Clear(); 
+
+WriteLine("Введите число А");
+int A = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите число B");
+int B = Convert.ToInt32(ReadLine());
+
+int Kub=A;
+for (int i=1; i<B; i++)
+{
+    Kub =Kub * A ;
+}
+WriteLine("Число А в степени B = " + Kub );
